3f62ddbc11d51c2d7ef12147337c3646b394a548
HarreshYohan <baptistyohan08@gmail.com>
2020-07-10 10:48:13 +0530

    After Editing Option

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 
 const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed': ''}>{props.todo.todo_priority}</td>
         <td>
-            <Link to={"/edit/"+props.todo._id}></Link>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
         
     </tr>
@@ -34,6 +34,20 @@ export default class TodoList extends Component{
             .catch(function (error){
                 console.log('error'); 
             })
+    }
+
+
+    componentDidUpdate(){
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({
+                    todos: response.data
+                });
+            })
+            .catch(function (error){
+                console.log('error'); 
+            })
+
     }
 
     todoList() {
